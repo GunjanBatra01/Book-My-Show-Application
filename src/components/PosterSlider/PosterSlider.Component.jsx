9eb5eb93b68caed871c7c32159169ba5f2b0446d
GunjanBatra01 <gunjan.batra0109@gmail.com>
2021-09-12 13:07:46 +0530

    completed project

---
+++ src/components/PosterSlider/PosterSlider.Component.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+import Slider from "react-slick";
+import Poster from "../Poster/Poster.Component";
+
+const PosterSlider = (props) => {
+
+    const settings = {
+        infinite: false,
+        speed: 500,
+        slidesToShow: 5,
+        slidesToScroll: 4,
+        InitialSlide: 0,
+        responsive: [
+            {
+                breakpoints: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                },
+            },
+            {
+                breakpoints: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    InitialSlide: 2,
+                },
+            },
+            {
+                breakpoints: 480,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                },
+            },
+        ],
+    };
+
+    const { posters, title, subtitle, isDark, config } = props;
+
+    const currentSettings = config ? config : settings;
+
+    return (
+        <>
+            <div className='flex flex-col items-start sm:ml-3 ml-0 my-2'>
+                <h3 
+                    className={`text-2xl font-bold ${isDark ? "text-white" : "text-black"}`}>
+                    {title}
+                </h3>
+                <p className={`text-sm ${isDark ? "text-white" : "text-gray-800"} `}>
+                    {subtitle}
+                </p>
+            </div>
+            <Slider {...currentSettings}>
+                {posters.map((each) => (
+                    <Poster {...each} isDark={isDark} />
+                ))}
+                {console.log(posters)}
+            </Slider>
+        </>
+    );
+};
+
+export default PosterSlider;
